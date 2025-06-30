935bc4445dc8e42dfd100031745349ecadfe2097
Johnatan-boot <johnatan.quenes@gmail.com>
2025-06-30 03:53:53 +0000

    configurando servicos rota

--- src/views/components/headerComponents.js
+++ src/views/components/headerComponents.js
@@ -3,19 +3,19 @@ class HeaderComponent extends HTMLElement {
     this.innerHTML = `
       <header>
         <div class="header__img">
-          <img class="img__header" src="public/assets/matrix.jpg" alt="Imagem de fundo" />
+          <img class="img__header" src="/assets/matrix.jpg" alt="Imagem de fundo" />
         </div>
         <div class="header__logo">
           <a href="/">
-            <img src="public/assets/logo.png" alt="Logo MasterCode">
+            <img src="/assets/logo.png" alt="Logo MasterCode">
           </a>
         </div>
         <h1>Bem-vindo à Mastercode — Empresa de Software</h1>
         <nav>
           <ul>
-            <li><a href="/views/home">Home</a></li>
-            <li><a href="/views/servicos">Serviços</a></li>
-            <li><a href="/views/form-email">Contato</a></li>
+            <li><a href="/home">Home</a></li>
+            <li><a href="/servicos">Serviços</a></li>
+            <li><a href="/form-email">Contato</a></li>
           </ul>
         </nav>
       </header>
